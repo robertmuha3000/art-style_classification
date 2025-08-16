16623350e152f295d64a7f1d573e347dd147b0cd
Arseni Muravjov <arsenimuravjov@MacBook-Air-Arseni.local>
2025-08-17 01:09:44 +0300

    fixed model_path typo

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ HEAD_EPOCHS = 3
 TRAIN_EPOCHS = 12
 CSV_PATH = "data/classes.csv"
 IMAGE_ROOT = "painting_classify/archive-3/"
-MODEL_PATH = "models/resnet34_wikiart_class_uncertainty.pth"
+MODEL_PATH = "models/resnet34_wikiart_uncertainty.pth"
 LABEL_ENCODER_PATH = "models/label_encoder.pkl"
 TRAINING_LOSS = torch.nn.CrossEntropyLoss(label_smoothing=0.1)
